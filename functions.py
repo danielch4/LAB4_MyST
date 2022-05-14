fdc4c327112c870d38710a94f7b35c98c6863a97
danielch4 <chavezd020@gmail.com>
2022-05-13 23:57:06 -0500

    Entrega Final

--- functions.py
+++ functions.py
@@ -8,3 +8,41 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+from datetime import timedelta, datetime
+import pandas as pd
+import numpy as np
+
+
+def f_compare_ts(ts_list_o, ts_list_d):
+    ts_list_o_dt = [datetime.strptime(i, "%Y-%m-%dT%H:%M:%S.%fZ") for i in ts_list_o]
+    ts_list_d_dt = [datetime.strptime(i, "%Y-%m-%dT%H:%M:%S.%fZ") for i in ts_list_d]
+    f_compare_ts = {}
+    f_compare_ts['first_o'] = min(ts_list_o_dt).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    f_compare_ts['last_o'] = max(ts_list_o_dt).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    f_compare_ts['qty_o'] = len(ts_list_o_dt)
+    f_compare_ts['first_d'] = min(ts_list_d_dt).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    f_compare_ts['last_d'] = max(ts_list_d_dt).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    f_compare_ts['qty_d'] = len(ts_list_d_dt)
+    unique_dates = list(dict.fromkeys(ts_list_o_dt + ts_list_d_dt))
+    exact_matches = [i for i in unique_dates if (i in (ts_list_o_dt) and i in (ts_list_d_dt))]
+    f_compare_ts['exact_match'] = {"qty": len(exact_matches), "values": exact_matches}
+    return f_compare_ts
+
+
+def historical_spread(ob_data):
+    k = list(ob_data.keys())
+    lowest_asks = []
+    highest_bids = []
+    spreads = []
+    for i in k:
+        lowest_asks.append(ob_data[i].ask.min())
+        highest_bids.append(ob_data[i].bid.max())
+        spreads.append(abs(ob_data[i].ask.min() - ob_data[i].bid.max()))
+
+    df_ts_tob = pd.DataFrame()
+    df_ts_tob["timestamp"] = k
+    df_ts_tob["bid"] = highest_bids
+    df_ts_tob["ask"] = lowest_asks
+    df_ts_tob["spread"] = spreads
+    return df_ts_tob
--- visualizations.py
+++ visualizations.py
@@ -8,3 +8,45 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+from datetime import timedelta, datetime
+
+
+def plot_orderbook(ob_data):
+    k = list(ob_data.keys())
+    max_volume = 0
+    max_timestamp = k[-1]
+    for i in k:
+        volume = ob_data[i].bid_size.sum() + ob_data[i].ask_size.sum()
+        if volume >= max_volume:
+            max_volume = volume
+            max_timestamp = i
+
+    bids = ob_data[max_timestamp][["bid_size", "bid"]].rename(columns={"bid_size": "Size", "bid": "Value"})
+    bids["label"] = "Bid"
+    asks = ob_data[max_timestamp][["ask_size", "ask"]].rename(columns={"ask_size": "Size", "ask": "Value"})
+    asks["label"] = "Ask"
+    bids = bids.sort_values(by=['Value'], ascending=True)
+    asks = asks.sort_values(by=['Value'], ascending=True)
+    long_df = pd.concat([bids, asks])
+    fig = px.bar(long_df, x="Value", y="Size", color="label", title="OrderBook TimeStamp: " + max_timestamp)
+    fig.update_xaxes(type='category')
+    fig.show()
+
+
+def plot_line_ts(df_ts_tob):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_ts_tob['timestamp'], y=df_ts_tob['bid'], name='Bid', mode='lines'))
+    fig.add_trace(go.Scatter(x=df_ts_tob['timestamp'], y=df_ts_tob['ask'], name='Ask', mode='lines'))
+    fig.update_layout(title='SPREAD GRAPH')
+    fig.show()
+
+
+def plot_bar_ts(df_ts_tob):
+    df_ts_tob['timestamp'] = df_ts_tob['timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
+    df_ts_tob['hour'] = df_ts_tob['timestamp'].dt.hour
+    fig = px.box(df_ts_tob, x="hour", y="spread")
+    return fig, df_ts_tob
